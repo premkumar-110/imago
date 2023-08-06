2c1687a990947c0fde5e1ed6c81336b65ee0b38c
Premkumar P <122764871+premkumar-110@users.noreply.github.com>
2023-08-06 14:14:42 +0530

    Update header.js

--- src/header/header.js
+++ src/header/header.js
@@ -8,43 +8,37 @@ import { FaUserCircle } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import '../header/header.css';
-const Header = ({ productID, setproductID}) => {
+
+const Header = ({ productID, setproductID }) => {
   const [user, setUser] = useState(null);
   const [showDropDown, setShowDropDown] = useState(false);
   const navigate = useNavigate();
   const [searchitem, setSearchItem] = useState('');
   const [filtereditems, setFilterItems] = useState([]);
   const [productsList, setProduct] = useState([]);
-  useEffect(() => {
 
-    const products = async () => {
-      const productData = await axios.get('https://server-imago.vercel.app/api/users/getProducts');
-      setProduct(productData.data.response);
-      
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await axios.get('https://server-imago.vercel.app/api/users/getProducts');
+        setProduct(response.data.response);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
     };
-    products();
+    getProducts();
   }, []);
+
   useEffect(() => {
     const getUser = async () => {
-      await fetch("https://server-imago.vercel.app/auth/login/success", {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
-        },
-      })
-        .then((response) => {
-          if (response.status === 200) return response.json();
-          throw new Error("Authentication has failed!");
-        })
-        .then((resObject) => {
-          setUser(resObject.user);
-        })
-        .catch((err) => {
-          console.log(err);
+      try {
+        const response = await axios.get("https://server-imago.vercel.app/auth/login/success", {
+          withCredentials: true, // Use "withCredentials" to include cookies in the request
         });
+        setUser(response.data.user);
+      } catch (error) {
+        console.error('Error fetching user:', error);
+      }
     };
     getUser();
   }, []);
@@ -53,64 +47,55 @@ const Header = ({ productID, setproductID}) => {
     const response = productsList?.filter((item) => {
       return item.title.toLowerCase().includes(searchitem.toLowerCase());
     });
-    console.log(response)
     setFilterItems(response || []);
   }, [searchitem, productsList]);
-  
 
   const handleLogout = () => {
     window.location.href = "http://localhost:3000/login";
   };
-  const handleGetProduct = async (id) => {
-    setSearchItem('')
+
+  const handleGetProduct = (id) => {
+    setSearchItem('');
     sessionStorage.setItem('id', id);
     setproductID(id);
     console.log(id);
     navigate('/product');
   };
+
   return (
     <>
       <div className="navbar">
-        <div className='logo' onClick={()=>navigate('/home')}><img src={logo} alt="logo"></img>IMAGO</div>
+        <div className='logo' onClick={() => navigate('/home')}><img src={logo} alt="logo"></img>IMAGO</div>
         <div className='search'>
           <input type='text' placeholder='Search your products' onChange={(e) => { setSearchItem(e.target.value) }} value={searchitem}></input><AiOutlineSearch className='searchLogo' />
         </div>
         {user && (
-          <div className='ProfileDetails' onClick={() => {
-            setShowDropDown(!showDropDown);
-          }}>
+          <div className='ProfileDetails' onClick={() => setShowDropDown(!showDropDown)}>
             <section className='nameData'>{user.displayName}</section>
-            <img
-              src={user.photos[0]?.value || ''}
-              alt="userLogo" 
-              className="avatar"
-            />
+            <img src={user.photos[0]?.value || ''} alt="userLogo" className="avatar" />
           </div>
         )}
       </div>
       {showDropDown && <div className='ProfileDropDown'>
         <ul>
           <li onClick={() => { navigate('/profile') }}><FaUserCircle className='DropDownLogo' />View Profile</li>
-          <li  onClick={()=>{navigate('/cart')}} ><ImCart className='DropDownLogo'/>View Cart</li>
+          <li onClick={() => { navigate('/cart') }}><ImCart className='DropDownLogo' />View Cart</li>
           <li onClick={handleLogout}><BiLogOut className='DropDownLogo' />Logout</li>
         </ul>
       </div>}
-      {
-        searchitem.length !== 0 && <>
-         <div className='SearchFilter' >
+      {searchitem.length !== 0 && <>
+        <div className='SearchFilter'>
           {
-           
             filtereditems.map((item) => {
               return (
-                <div className='SerachItem' key={item.id} onClick={()=>{handleGetProduct(item.id)}}>
+                <div className='SerachItem' key={item.id} onClick={() => { handleGetProduct(item.id) }}>
                   {item.title}
                 </div>
               )
             })
           }
-          </div>
-        </>
-      }
+        </div>
+      </>}
     </>
   )
 }

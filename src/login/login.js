91b36a4d917d6e63f0cf798ebe590bea5c118c98
premkumar-110 <websitehostformysite@gmail.com>
2023-11-10 18:23:47 +0530

    Published

--- src/login/login.js
+++ src/login/login.js
@@ -74,7 +74,7 @@ const Login = ({ setUserEmail }) => {
     .then(async (result) => {
       const user = result.user;
       setUser(user);
-      const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}api/users/googleLogin`,{email:user.email,password:'123abc@123'});
+      const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}api/users/login`,{email:user.email,password:'123abc@123'});
       if(response.status==200){
         const token = response.data.token; // Update this field name based on the actual response
         Cookies.set("user_id", token);
